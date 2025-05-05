f9f3a7e313e336cceec134ee90a04f20b04f8fbc
Tim Stavenger <timothy.stavenger@pnnl.gov>
2025-05-05 08:41:14 -0700

    add comments to test batch script

--- test.bat
+++ test.bat
@@ -1,7 +1,13 @@
+@REM Set up a clean Python virtual environment for the test.
+@REM Really this only needs to be done when the dependencies change.
 rmdir /S /q venv
 python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+@REM Run the Python file with some bogus input that doesn't fail.
 python 3_chromophore_vibronic_simulation.py output 2 2 2 2 2 2 2 2 2 2 2 2 2
+
+@REM Deactivate the virtual environment when we're done.
 deactivate
